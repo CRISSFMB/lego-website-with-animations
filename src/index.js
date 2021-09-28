9f9f5fbf1e791fd1306793fdbca6bdea3165e1e4
cristian mosquera <cmosqbone1@gmail.com>
2021-09-28 23:33:47 +0200

    add modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./scss/styles.scss";
+
+// variables
+
+
+const hero = document.querySelectorAll(".footer__hero-content")
+const modal = document.querySelector(".modal")
+const close = document.querySelector(".modal__icon")
+
+// listeners
+hero.forEach( item => {
+    item.addEventListener("click",openModal )
+})
+
+close.addEventListener('click', closeModal);
+
+//functions
+function openModal () {
+    modal.classList.remove('hidden')
+    modal.classList.add('visibility')
+}
+
+function closeModal (){
+    modal.classList.remove('visibility')
+    modal.classList.add('hidden')
+
+    
+}
+
+
+
+
